c9bc92cf2718e11dab2ad753631994f6b9204758
kiminzajnr <kiminzaeric@gmail.com>
2020-09-09 13:44:45 +0300

    update service worker

--- sw.js
+++ sw.js
@@ -2,21 +2,21 @@ self.addEventListener('install', (event)=>{
 	event.waitUntil(
 		caches.open('v1').then((cache)=>{
 			return cache.addAll([
-				'/',
-				'index.html',
-				'style.css',
-				'weather.js',
-				'apple-touch-icon.png',
-				'favicon-32x32.png',
-				'android-chrome-192x192.png',
-				'android-chrome-144x144',
-				'android-chrome-256x256',
-				'android-chrome-36x36',
-				'android-chrome-384x384',
-				'android-chrome-48x48',
-				'android-chrome-72x72',
-				'android-chrome-96x96',
-				'favicon-16x16.png'
+				'.//',
+				'./index.html',
+				'./style.css',
+				'./weather.js',
+				'./apple-touch-icon.png',
+				'./favicon-32x32.png',
+				'./android-chrome-192x192.png',
+				'./android-chrome-144x144',
+				'./android-chrome-256x256',
+				'./android-chrome-36x36',
+				'./android-chrome-384x384',
+				'./android-chrome-48x48',
+				'./android-chrome-72x72',
+				'./android-chrome-96x96',
+				'./favicon-16x16.png'
 				]);
 		})
 		);
